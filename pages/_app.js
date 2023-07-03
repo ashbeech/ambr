27c3352aeaea60059a4c8ec912a93fa5c83b26eb
Ash Beech <ash@bullish.design>
2023-07-03 22:56:38 +0100

    Debugging CSS: mobile debugging.

--- pages/_app.js
+++ pages/_app.js
@@ -185,7 +185,27 @@ export default function AmbrApp({ Component, pageProps, err }) {
 const App = (props) => {
   const browser = useBrowser();
   const toast = useToast();
-  screen.orientation.lock("portrait");
+  const isLandscape = () =>
+      window.matchMedia("(orientation:landscape)").matches,
+    [orientation, setOrientation] = useState(
+      isLandscape() ? "landscape" : "portrait"
+    ),
+    onWindowResize = () => {
+      clearTimeout(window.resizeLag);
+      window.resizeLag = setTimeout(() => {
+        delete window.resizeLag;
+        setOrientation(isLandscape() ? "landscape" : "portrait");
+      }, 200);
+    };
+
+  useEffect(
+    () => (
+      onWindowResize(),
+      window.addEventListener("resize", onWindowResize),
+      () => window.removeEventListener("resize", onWindowResize)
+    ),
+    []
+  );
 
   // Periodically check if this release is bad so we can reload the browser
   useEffect(() => {
